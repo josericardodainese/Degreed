99c67cc81d50d15fe36112574b7ae304b3043967
Ricardo Dainese <josericardodainese@gmail.com>
2021-08-05 22:57:00 -0300

    First Script

---
+++ Groovy/src/Application.groovy
@@ -0,0 +1,12 @@
+class Application {
+
+    static void sum(int numOne, int numTwo) {
+        int result = numOne + numTwo
+        println(result)
+    }
+
+    static void main(String[] args) {
+        sum(123,66)
+    }
+
+}
